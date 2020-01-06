bc6df9dd1ac3aa9524eb12582cc2f2984a0bd63e
datdofpt <dathalongbay@gmail.com>
2020-01-06 14:15:17 +0700

    Create index1.java

---
+++ 001/index1.java
@@ -0,0 +1,21 @@
+public class MyClass {
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      if (i == 4) {
+        break;
+      }
+      System.out.println(i);
+    }  
+  }
+}
+
+public class MyClass {
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      if (i == 0) {
+        break;
+      }
+    }
+    System.out.println(i);
+  }
+}
